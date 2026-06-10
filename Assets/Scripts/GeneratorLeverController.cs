8b4fdfddfbc2b5f9b157542ab0cc28dcfe0f2d08
itpakhs04-maker <it.pakhs04@gmail.com>
2026-06-10 18:48:06 +0900

    APK다운로드
    미누

--- Assets/Scripts/GeneratorLeverController.cs
+++ Assets/Scripts/GeneratorLeverController.cs
@@ -1,175 +1,103 @@
 ﻿using UnityEngine;
-using UnityEngine.InputSystem;
+using UnityEngine.Events;
 
-public class GeneratorLeverInteraction : MonoBehaviour
+public class GeneratorLeverController : MonoBehaviour
 {
-    [Header("References")]
-    public Transform pivot;
-    public GeneratorLeverController generator;
+    [Header("State")]
+    public bool isActivated = false;
 
-    [Header("Hands")]
-    public Transform leftHand;
-    public Transform rightHand;
-    public InputActionProperty leftGrip;
-    public InputActionProperty rightGrip;
+    [Header("Objects To Enable When Activated")]
+    public GameObject[] objectsToEnable;
 
-    [Header("Lever Setting")]
-    public float grabRange = 0.45f;
-    public Vector3 localAxis = Vector3.right;
-    public float pulledAngle = -70f;
-    public float pullDistance = 0.35f;
-    public float activateRatio = 0.9f;
-    public bool stayDownAfterActivated = true;
+    [Header("Objects To Disable When Activated")]
+    public GameObject[] objectsToDisable;
 
-    private Vector3 startLocalPosition;
-    private Quaternion startLocalRotation;
+    [Header("Lights To Turn On")]
+    public Light[] lightsToTurnOn;
 
-    private Transform activeHand;
-    private Vector3 grabStartHandLocalPos;
+    [Header("Renderer Emission")]
+    public Renderer[] emissionRenderers;
+    public Color emissionColor = Color.yellow;
+    public float emissionIntensity = 2.5f;
 
-    private bool isGrabbed;
-    private bool isActivated;
+    [Header("Audio")]
+    public AudioSource audioSource;
+    public AudioClip activateSound;
 
-    private void Awake()
+    [Header("Events")]
+    public UnityEvent onActivated;
+
+    public void ActivateGenerator()
     {
-        if (pivot == null)
-            pivot = transform.parent;
-
-        if (generator == null)
-            generator = GetComponentInParent<GeneratorLeverController>();
-
-        startLocalPosition = transform.localPosition;
-        startLocalRotation = transform.localRotation;
-
-        Rigidbody rb = GetComponent<Rigidbody>();
-        if (rb != null)
-        {
-            rb.useGravity = false;
-            rb.isKinematic = true;
-        }
-    }
-
-    private void OnEnable()
-    {
-        leftGrip.action?.Enable();
-        rightGrip.action?.Enable();
-    }
-
-    private void Update()
-    {
-        if (isActivated && stayDownAfterActivated)
-        {
-            SetLeverRatio(1f);
+        if (isActivated)
             return;
+
+        isActivated = true;
+
+        Debug.Log("[Generator] Activated");
+
+        for (int i = 0; i < objectsToEnable.Length; i++)
+        {
+            if (objectsToEnable[i] != null)
+                objectsToEnable[i].SetActive(true);
         }
 
-        if (!isGrabbed)
+        for (int i = 0; i < objectsToDisable.Length; i++)
         {
-            TryStartGrab();
+            if (objectsToDisable[i] != null)
+                objectsToDisable[i].SetActive(false);
         }
-        else
+
+        for (int i = 0; i < lightsToTurnOn.Length; i++)
         {
-            UpdateGrab();
+            if (lightsToTurnOn[i] != null)
+                lightsToTurnOn[i].enabled = true;
+        }
+
+        ApplyEmission();
+
+        if (audioSource != null && activateSound != null)
+            audioSource.PlayOneShot(activateSound);
+
+        onActivated?.Invoke();
+    }
+
+    private void ApplyEmission()
+    {
+        for (int i = 0; i < emissionRenderers.Length; i++)
+        {
+            Renderer targetRenderer = emissionRenderers[i];
+
+            if (targetRenderer == null)
+                continue;
+
+            Material mat = targetRenderer.material;
+
+            if (mat == null)
+                continue;
+
+            mat.EnableKeyword("_EMISSION");
+            mat.SetColor("_EmissionColor", emissionColor * emissionIntensity);
         }
     }
 
-    private void TryStartGrab()
+    [ContextMenu("Test Activate Generator")]
+    public void TestActivateGenerator()
     {
-        if (leftHand != null && IsGripPressed(leftGrip) && IsHandNear(leftHand))
+        ActivateGenerator();
+    }
+
+    [ContextMenu("Reset Generator")]
+    public void ResetGenerator()
+    {
+        isActivated = false;
+
+        for (int i = 0; i < lightsToTurnOn.Length; i++)
         {
-            StartGrab(leftHand);
-            return;
+            if (lightsToTurnOn[i] != null)
+                lightsToTurnOn[i].enabled = false;
         }
 
-        if (rightHand != null && IsGripPressed(rightGrip) && IsHandNear(rightHand))
-        {
-            StartGrab(rightHand);
-            return;
-        }
-    }
-
-    private void StartGrab(Transform hand)
-    {
-        activeHand = hand;
-        isGrabbed = true;
-
-        grabStartHandLocalPos = pivot.InverseTransformPoint(activeHand.position);
-
-        Debug.Log("[Lever] 레버 잡힘: " + activeHand.name);
-    }
-
-    private void UpdateGrab()
-    {
-        if (activeHand == null)
-        {
-            EndGrab();
-            return;
-        }
-
-        bool gripStillPressed =
-            activeHand == leftHand ? IsGripPressed(leftGrip) : IsGripPressed(rightGrip);
-
-        if (!gripStillPressed)
-        {
-            EndGrab();
-            return;
-        }
-
-        Vector3 currentHandLocalPos = pivot.InverseTransformPoint(activeHand.position);
-
-        float pullAmount = grabStartHandLocalPos.y - currentHandLocalPos.y;
-        float ratio = Mathf.Clamp01(pullAmount / pullDistance);
-
-        SetLeverRatio(ratio);
-
-        if (!isActivated && ratio >= activateRatio)
-        {
-            isActivated = true;
-            Debug.Log("[Lever] 발전기 작동");
-
-            if (generator != null)
-                generator.ActivateGenerator();
-
-            if (stayDownAfterActivated)
-                SetLeverRatio(1f);
-        }
-    }
-
-    private void EndGrab()
-    {
-        Debug.Log("[Lever] 레버 놓음");
-
-        isGrabbed = false;
-        activeHand = null;
-
-        if (isActivated && stayDownAfterActivated)
-            SetLeverRatio(1f);
-        else
-            SetLeverRatio(0f);
-    }
-
-    private bool IsHandNear(Transform hand)
-    {
-        float distance = Vector3.Distance(hand.position, transform.position);
-        return distance <= grabRange;
-    }
-
-    private bool IsGripPressed(InputActionProperty action)
-    {
-        if (action.action == null)
-            return false;
-
-        return action.action.ReadValue<float>() > 0.5f;
-    }
-
-    private void SetLeverRatio(float ratio)
-    {
-        ratio = Mathf.Clamp01(ratio);
-
-        Quaternion deltaRotation =
-            Quaternion.AngleAxis(pulledAngle * ratio, localAxis.normalized);
-
-        transform.localPosition = deltaRotation * startLocalPosition;
-        transform.localRotation = deltaRotation * startLocalRotation;
+        Debug.Log("[Generator] Reset");
     }
 }
